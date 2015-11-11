4f543e6d346a344de265c3a9d9391a9c74597d88
RyodoTanaka <k104073r@mail.kyutech.jp>
2015-11-11 21:43:52 +0000

    [fix] rosaddress コマンドを追加
    rosaddress client <server_address>
    rosaddress server
    rosaddress local
    
    以上のコマンドがある

--- scripts/rosaddress.bash
+++ scripts/rosaddress.bash
@@ -1,9 +1,5 @@
 #!/bin/bash
-rosserver() {
-	# eth0とwlan0の現在のIPアドレスを取得
-	eth0_addr=$(ip -f inet -o addr show eth0|cut -d\  -f 7 | cut -d/ -f 1)
-	wlan0_addr=$(ip -f inet -o addr show wlan0|cut -d\  -f 7 | cut -d/ -f 1)
-
+func_rosserver() {
 	if [ -z "$eth0_addr" ]; then
 		echo "No eth0 connection..."
 		if [ -z  "$wlan0_addr" ]; then
@@ -21,14 +17,14 @@ rosserver() {
 			export ROS_MASTER_URI=http://${wlan0_addr}:11311
 			export ROS_HOST_NAME=${wlan0_addr}
 			export ROS_IP=${wlan0_addr}
-			export PS1="\[\033[44;1;37m\]<ROS_server>\[\033[0m\]\w$ "
+			export PS1="\[\033[41;1;33m\]<ROS_server>\[\033[0m\]\w$ "
 		fi	
 	else
 		echo "There are eth0 connection"
 		export ROS_MASTER_URI=http://${eth0_addr}:11311
 		export ROS_HOST_NAME=${eth0_addr}
 		export ROS_IP=${eth0_addr}
-		export PS1="\[\033[44;1;37m\]<ROS_server>\[\033[0m\]\w$ "
+		export PS1="\[\033[41;1;33m\]<ROS_server>\[\033[0m\]\w$ "
 	fi
 
 	env | grep "ROS_MASTER_URI"
@@ -36,12 +32,9 @@ rosserver() {
 	env | grep "ROS_IP"
 }
 
-rosclient() {
-	# eth0とwlan0の現在のIPアドレスを取得
-	eth0_addr=$(ip -f inet -o addr show eth0|cut -d\  -f 7 | cut -d/ -f 1)
-	wlan0_addr=$(ip -f inet -o addr show wlan0|cut -d\  -f 7 | cut -d/ -f 1)
-
+func_rosclient() {
 	if [ -z "$1" ]; then
+		echo $1
 		echo "Input the ROS server's IP address.'"
 	else
 		if [ -z "$eth0_addr" ]; then
@@ -61,14 +54,14 @@ rosclient() {
 				export ROS_MASTER_URI=http://$1:11311
 				export ROS_HOST_NAME=${wlan0_addr}
 				export ROS_IP=${wlan0_addr}
-				export PS1="\[\033[42;1;37m\]<ROS_client>\[\033[0m\]\w$ "
+				export PS1="\[\033[44;1;33m\]<ROS_client>\[\033[0m\]\w$ "
 			fi	
 		else
 			echo "There are eth0 connection"
 			export ROS_MASTER_URI=http://$1:11311
 			export ROS_HOST_NAME=${eth0_addr}
 			export ROS_IP=${eth0_addr}
-			export PS1="\[\033[44;1;37m\]<ROS_client>\[\033[0m\]\w$ "
+			export PS1="\[\033[44;1;33m\]<ROS_client>\[\033[0m\]\w$ "
 		fi
 		env | grep "ROS_MASTER_URI"
 		env | grep "ROS_HOST_NAME"
@@ -76,15 +69,37 @@ rosclient() {
 	fi
 }
 
-roslocal() {
+func_roslocal() {
 	export ROS_MASTER_URI=http://localhost:11311
 	unset ROS_HOST_NAME
 	unset ROS_IP
+	export PS1="\[\033[42;1;33m\]<ROS_local>\[\033[0m\]\w$ "
 	env | grep "ROS_MASTER_URI"
 	env | grep "ROS_HOST_NAME"
 	env | grep "ROS_IP"
 }
 
-alias rosserver=rosserver
-alias rosclient=rosclient
-alias roslocal=roslocal
+function _func_comp_rosaddress(){
+	local cur=${COMP_WORDS[COMP_CWORD]}
+	if [ "$COMP_CWORD" -eq 1 ]; then
+		COMPREPLY=( $(compgen -W "server client local" -- $cur) )
+	fi
+}
+
+function func_rosaddress() {
+	# Get now eth0 or wlan0 IP address
+	eth0_addr=$(ip -f inet -o addr show eth0|cut -d\  -f 7 | cut -d/ -f 1)
+	wlan0_addr=$(ip -f inet -o addr show wlan0|cut -d\  -f 7 | cut -d/ -f 1)
+
+	if [ $1 = "local" ]; then
+		func_roslocal
+	elif [ $1 = "server" ]; then
+		func_rosserver
+	elif [ $1 = "client" ]; then
+		func_rosclient $2
+	fi
+	
+}
+
+alias rosaddress=func_rosaddress
+complete -o default -F _func_comp_rosaddress rosaddress
