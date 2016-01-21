2aa1cbbe38f2baa6a66ede80b3cad7af86ced0f6
RyodoTanaka <k104073r@mail.kyutech.jp>
2016-01-22 01:12:46 +0900

    [modify] 5秒おきにON・OFFするソース

--- motoman_gripper/Arduino/JammingTest/jammingtest.ino
+++ motoman_gripper/Arduino/JammingTest/jammingtest.ino
@@ -1,16 +1,15 @@
-int switch_pin = 8;
+int swicth_pin = 8;
 
 void setup()
 {
   pinMode(switch_pin, OUTPUT);
   digitalWrite(switch_pin, LOW);
-  delay(2000);
 }
 
 void loop()
 {
-  digitalWrite(switch_pin,HIGH); // switch ON
+  digitalWrite(switch_pin, HIGH); // switch on
   delay(5000);
-  digitalWrite(switch_pin,LOW); // switch OFF
+  digitalWrite(switch_pin, LOW); // switch off
   delay(5000);
 }
