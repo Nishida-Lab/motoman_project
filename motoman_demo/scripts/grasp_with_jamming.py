b1cd7eddc8d8c0df4f5cdd5d09edbde5e7b08d85
RyodoTanaka <k104073r@mail.kyutech.jp>
2016-02-19 21:34:15 +0900

    [modify] 把持のデモプログラムを追加

--- motoman_demo/scripts/grasp_with_jamming.py
+++ motoman_demo/scripts/grasp_with_jamming.py
@@ -51,47 +51,96 @@ def main():
             target_pose.orientation.y = tar_q[1]
             target_pose.orientation.z = tar_q[2]
             target_pose.orientation.w = tar_q[3]
-            # target_pose.orientation.x = trans.transform.rotation.x
-            # target_pose.orientation.y = trans.transform.rotation.y
-            # target_pose.orientation.z = trans.transform.rotation.z
-            # target_pose.orientation.w = trans.transform.rotation.w
             print target_pose
             arm.set_pose_target(target_pose)
             arm.go()
+            arm.clear_pose_targets()
             
             # Get Grasp
+            # waypoints = []
+            
+            # waypoints.append(arm.get_current_pose().pose)
+
+            # wpose = geometry_msgs.msg.Pose()
+            # wpose.orientation.w = 1.0
+            # wpose.position.x = waypoints[0].position.x
+            # wpose.position.y = waypoints[0].position.y - 0.085
+            # wpose.position.z = waypoints[0].position.z
+            # waypoints.append(copy.deepcopy(wpose))
+            # (plan, fraction) = arm.compute_cartesian_path(waypoints, 0.005, 0.0, False)
+
+            
             target_pose.position.x = trans.transform.translation.x - 0.004
             target_pose.position.y = trans.transform.translation.y
-            target_pose.position.z = trans.transform.translation.z + 0.22
+            target_pose.position.z = trans.transform.translation.z + 0.23
             target_pose.orientation = target_pose.orientation
-            # target_pose.orientation.x = trans.transform.rotation.x
-            # target_pose.orientation.y = trans.transform.rotation.y
-            # target_pose.orientation.z = trans.transform.rotation.z
-            # target_pose.orientation.w = trans.transform.rotation.w
             print target_pose
             arm.set_pose_target(target_pose)
             arm.go()
-            
+            arm.clear_pose_targets()
+
             # Grasp
             grasp_pub.publish(1)
             print "!! Grasping !!"
             rospy.sleep(1.0)
-            # Go to Home Position
-            target_pose.position.x = 0.503
-            target_pose.position.y = 0
-            target_pose.position.z = 1.1563
-            target_pose.orientation.x = 0
-            target_pose.orientation.y = 0
-            target_pose.orientation.z = 0
-            target_pose.orientation.w = 1
+
+            # Start Return
+            # waypoints = []
+            
+            # waypoints.append(arm.get_current_pose().pose)
+            
+            # wpose = geometry_msgs.msg.Pose()
+            # wpose.orientation.w = 1.0
+            # wpose.position.x = waypoints[0].position.x
+            # wpose.position.y = waypoints[0].position.y + 0.085
+            # wpose.position.z = waypoints[0].position.z
+            # waypoints.append(copy.deepcopy(wpose))
+            # (plan, fraction) = arm.compute_cartesian_path(waypoints, 0.005, 0.0, False)
+            target_pose.position.x = trans.transform.translation.x - 0.004
+            target_pose.position.y = trans.transform.translation.y
+            target_pose.position.z = trans.transform.translation.z + 0.32
+            target_pose.orientation = target_pose.orientation
+            print target_pose
             arm.set_pose_target(target_pose)
             arm.go()
+            arm.clear_pose_targets()
+            
+
+            # Go to Home Position
+            target_pose.position.x = trans.transform.translation.x - 0.004
+            target_pose.position.y = -trans.transform.translation.y
+            target_pose.position.z = trans.transform.translation.z + 0.32
+            target_pose.orientation = target_pose.orientation
+            arm.set_pose_target(target_pose)
+            arm.go()
+            arm.clear_pose_targets()
+
+            
+            target_pose.position.x = trans.transform.translation.x - 0.004
+            target_pose.position.y = -trans.transform.translation.y
+            target_pose.position.z = trans.transform.translation.z + 0.25
+            target_pose.orientation = target_pose.orientation
+            arm.set_pose_target(target_pose)
+            arm.go()
+            arm.clear_pose_targets()
+            
             
             # Release
             print " !! Release !!"
             grasp_pub.publish(0)
             rospy.sleep(2)
             get_tf_flg = True
+
+            target_pose.position.x = 0.503
+            target_pose.position.y = 0
+            target_pose.position.z = 0.6563
+            target_pose.orientation.x = 0
+            target_pose.orientation.y = 0
+            target_pose.orientation.z = 0
+            target_pose.orientation.w = 1
+            arm.set_pose_target(target_pose)
+            arm.go()
+            arm.clear_pose_targets()
             
         except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException) :
             continue
