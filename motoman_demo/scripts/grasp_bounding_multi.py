40e0299737056c3c9cd126e227837efa571995b4
Ry0 <k104037r@mail.kyutech.jp>
2016-05-26 17:26:23 +0900

    [add] bounding_boxの数を自動で認識してプランニングするデモを追加

--- motoman_demo/scripts/grasp_bounding_multi.py
+++ motoman_demo/scripts/grasp_bounding_multi.py
@@ -8,31 +8,18 @@ import copy
 import tf2_ros
 import tf
 from std_msgs.msg import Int32
+from jsk_recognition_msgs.msg import BoundingBoxArray
+from jsk_recognition_msgs.msg import BoundingBox
 
-def main():
-    # init node
-    rospy.init_node("grasp_commander")
 
-    # ========== publisher to jamming gripper ========== #
-    grasp_pub = rospy.Publisher('/jamming_grasp', Int32, queue_size=1)
-    
-    # ========== Moveit init ========== #
-    # moveit_commander init
-    robot = moveit_commander.RobotCommander()
-    arm = moveit_commander.MoveGroupCommander("arm")
-    arm_initial_pose = arm.get_current_pose().pose
-    target_pose = geometry_msgs.msg.Pose()
-
-    # Set the planning time
-    arm.set_planning_time(10.0)
-
+def get_tf_data(times):
     # ========== TF Lister ========== #
     tf_buffer = tf2_ros.Buffer()
     tf_listner = tf2_ros.TransformListener(tf_buffer)
 
     tf_time = rospy.Time(0)
     trans = []
-    for i in [0, 1, 2, 3, 4, 5, 6]:
+    for i in range(times):
         target = "object_" + str(i)
         get_tf_flg = False
         while not get_tf_flg :
@@ -42,48 +29,30 @@ def main():
 
             except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException) :
                 continue
-        
+    return trans
 
-    for i in [0, 1, 2, 3, 4, 5, 6]:
-        print "world -> object[" + str(i) + "]" 
+def cb_once(bbox_array_data):
+    # ========== publisher to jamming gripper ========== #
+    grasp_pub = rospy.Publisher('/jamming_grasp', Int32, queue_size=1)
+    
+    # ========== Moveit init ========== #
+    # moveit_commander init
+    robot = moveit_commander.RobotCommander()
+    arm = moveit_commander.MoveGroupCommander("arm")
+    arm_initial_pose = arm.get_current_pose().pose
+    target_pose = geometry_msgs.msg.Pose()
+    # Set the planning time
+    arm.set_planning_time(10.0)
+
+    trans = []
+    trans = get_tf_data(len(bbox_array_data.boxes))
+
+    for i in range(len(trans)):
+        print "world -> object[" + str(i) + "]"
         print trans[i].transform
-        print "Target Place & Pose"
-        # Go to up from target
-        # target_pose.position.x = trans[i].transform.translation.x
-        # target_pose.position.y = trans[i].transform.translation.y
-        # target_pose.position.z = trans[i].transform.translation.z + 0.30
-        # q = (trans[i].transform.rotation.x,
-        #      trans[i].transform.rotation.y,
-        #      trans[i].transform.rotation.z,
-        #      trans[i].transform.rotation.w)
-        # (roll,pitch,yaw) = tf.transformations.euler_from_quaternion(q)
-        # # roll -= math.pi/6.0
-        # pitch += math.pi/2.0
-        # # yaw += math.pi/4.0
-        # tar_q = tf.transformations.quaternion_from_euler(roll, pitch, yaw)
-        # target_pose.orientation.x = tar_q[0]
-        # target_pose.orientation.y = tar_q[1]
-        # target_pose.orientation.z = tar_q[2]
-        # target_pose.orientation.w = tar_q[3]
-        # print target_pose
-        # arm.set_pose_target(target_pose)
-        # arm.go()
-        # arm.clear_pose_targets()
-        #rospy.sleep(2)
-        # Get Grasp
-        # waypoints = []
-        # waypoints.append(arm.get_current_pose().pose)
-        # wpose = geometry_msgs.msg.Pose()
-        # wpose.orientation.w = 1.0
-        # wpose.position.x = waypoints[0].position.x
-        # wpose.position.y = waypoints[0].position.y - 0.085
-        # wpose.position.z = waypoints[0].position.z
-        # waypoints.append(copy.deepcopy(wpose))
-        # (plan, fraction) = arm.compute_cartesian_path(waypoints, 0.005, 0.0, False)
-            
         target_pose.position.x = trans[i].transform.translation.x
         target_pose.position.y = trans[i].transform.translation.y
-        target_pose.position.z = trans[i].transform.translation.z + 0.243
+        target_pose.position.z = trans[i].transform.translation.z + 0.2
         q = (trans[i].transform.rotation.x,
              trans[i].transform.rotation.y,
              trans[i].transform.rotation.z,
@@ -98,7 +67,6 @@ def main():
         target_pose.orientation.z = tar_q[2]
         target_pose.orientation.w = tar_q[3]
         target_pose.orientation = target_pose.orientation
-        # print target_pose
         arm.set_pose_target(target_pose)
         arm.go()
         arm.clear_pose_targets()
@@ -107,29 +75,7 @@ def main():
         print "!! Grasping !!"
         rospy.sleep(0.5)
     
-        # Start Return
-        # waypoints = []
-        
-        # waypoints.append(arm.get_current_pose().pose)
-        
-        # wpose = geometry_msgs.msg.Pose()
-        # wpose.orientation.w = 1.0
-        # wpose.position.x = waypoints[0].position.x
-        # wpose.position.y = waypoints[0].position.y + 0.085
-        # wpose.position.z = waypoints[0].position.z
-        # waypoints.append(copy.deepcopy(wpose))
-        # (plan, fraction) = arm.compute_cartesian_path(waypoints, 0.005, 0.0, False)
-        # target_pose.position.x = trans[i].transform.translation.x
-        # target_pose.position.y = trans[i].transform.translation.y + 0.04
-        # target_pose.position.z = trans[i].transform.translation.z + 0.32
-        # target_pose.orientation = target_pose.orientation
-        # print target_pose
-        # arm.set_pose_target(target_pose)
-        # arm.go()
-        # arm.clear_pose_targets()
-        #rospy.sleep(2)
-    
-        # Go to Home Position
+        # Go to Set Position
         target_pose.position.x = -0.13683 + (i-1)*0.08
         target_pose.position.y = -0.22166
         target_pose.position.z = 0.40554
@@ -140,22 +86,28 @@ def main():
         arm.set_pose_target(target_pose)
         arm.go()
         arm.clear_pose_targets()
-        #rospy.sleep(2)
 
-        # target_pose.position.x = target_pose.position.x
-        # target_pose.position.y = target_pose.position.y
-        # target_pose.position.z -= 0.10
-        # target_pose.orientation = target_pose.orientation
-        # arm.set_pose_target(target_pose)
-        # arm.go()
-        # arm.clear_pose_targets()
-                
         # Release
         print " !! Release !!"
         grasp_pub.publish(0)
         rospy.sleep(1)
- 
 
+    arm.set_pose_target(arm_initial_pose)
+    arm.go()
+    arm.clear_pose_targets
+        
+    subscriber.unregister()
+
+def main():
+    # init node
+    rospy.init_node("grasp_commander")
+
+    # ========= Call Back Function ========
+    sub_once = None
+    sub_once = rospy.Subscriber('/clustering_result', BoundingBoxArray, cb_once, sub_once)
+
+    rospy.spin()
+        
 if __name__ == '__main__':
     try:
         main()
