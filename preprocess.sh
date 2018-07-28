3471083fa33e2db4e5ad9796fa3967791f73794d
MoriKen254 <masamorobo@gmail.com>
2018-07-28 17:59:25 +0900

    [Fix] preprocess for travis

---
+++ preprocess.sh
@@ -0,0 +1,18 @@
+PWD_DIR=$(pwd)
+
+yes | apt-add-repository ppa:floe/libusb && apt update -qq && apt install -y libusb-1.0-0-dev
+apt install -y libturbojpeg libjpeg-turbo8-dev
+dpkg -i debs/libglfw3*deb; apt install -f; apt install -y libgl1-mesa-dri-lts-vivid
+yes | apt-add-repository ppa:pmjdebruijn/beignet-testing; apt update -qq; apt install -y beignet-dev;
+cd ${PWD_DIR}
+cd ../../../
+mkdir libs
+cd libs
+git clone https://github.com/OpenKinect/libfreenect2.git
+cd libfreenect2
+mkdir build && cd build
+cmake ..
+make
+make install
+ldconfig
+cd ${PWD_DIR}
