14c5582c1a63613796d731e579d117863283928b
MoriKen <p595201m@mail.kyutech.jp>
2018-08-06 18:20:40 +0900

    [Modify] Clean up preprocess

--- .preprocess.sh
+++ .preprocess.sh
@@ -36,49 +36,6 @@ echo "*****         for PCL 1.7         *****"
 echo "***************************************"
 echo "***** yes | apt-add-repository ppa:v-launchpad-jochen-sprickerhof-de/pcl; apt-get update -qq; apt-get install -y libpcl-all; *****"
 yes | apt-add-repository ppa:v-launchpad-jochen-sprickerhof-de/pcl; apt-get update -qq; apt-get install -y libpcl-all;
-#echo "***************************************"
-#echo "*****         for PCL 1.8         *****"
-#echo "***************************************"
-#echo "***** apt install gdebi -y *****"
-#apt install gdebi -y
-#echo "***** git clone https://github.com/MoriKen254/PCL-Linux-Release.git pcl-trunk *****"
-#git clone https://github.com/MoriKen254/PCL-Linux-Release.git pcl-trunk
-#echo "***** ls *****"
-#ls
-#echo "***** cd pcl-trunk *****"
-#cd pcl-trunk
-#echo "***** ls *****"
-#ls
-#echo "***** gdebi pcl_1.8.1-1_amd64.deb *****"
-#yes | gdebi pcl_1.8.1-1_amd64.deb
-
-#echo "***************************************"
-#echo "*****      Reinstall PCL 1.7      *****"
-#echo "***************************************"
-#apt remove -y libpcl-1.7-all libpcl-1.7-all-dev libpcl-1.7-bin;libpcl-1.7-doc
-#apt remove -y libpcl-apps-1.7 libpcl-apps-1.7-dev
-#apt remove -y libpcl-common-1.7 libpcl-common-1.7-dev
-#apt remove -y libpcl-features-1.7 libpcl-features-1.7-dev
-#apt remove -y libpcl-filters-1.7 libpcl-filters-1.7-dev
-#apt remove -y libpcl-geometry-1.7-dev libpcl-geometry-1.7-dev
-#apt remove -y libpcl-io-1.7 libpcl-io-1.7-dev
-#apt remove -y libpcl-kdtree-1.7 libpcl-kdtree-1.7-dev
-#apt remove -y libpcl-keypoints-1.7 libpcl-keypoints-1.7-dev
-#apt remove -y libpcl-octree-1.7 libpcl-octree-1.7-dev
-#apt remove -y libpcl-outofcore-1.7 libpcl-outofcore-1.7-dev
-#apt remove -y libpcl-people-1.7 libpcl-people-1.7-dev
-#apt remove -y libpcl-recognition-1.7 libpcl-recognition-1.7-dev
-#apt remove -y libpcl-registration-1.7 libpcl-registration-1.7-dev
-#apt remove -y libpcl-sample-consensus-1.7 libpcl-sample-consensus-1.7-dev
-#apt remove -y libpcl-search-1.7 libpcl-search-1.7-dev
-#apt remove -y libpcl-segmentation-1.7 libpcl-segmentation-1.7-dev
-#apt remove -y libpcl-surface-1.7 libpcl-surface-1.7-dev
-#apt remove -y libpcl-tracking-1.7 libpcl-tracking-1.7-dev
-#apt remove -y libpcl-visualization-1.7 libpcl-visualization-1.7-dev
-#apt install -y ros-indigo-jsk-interactive ros-indigo-jsk-interactive-marker
-#apt install -y ros-indigo-jsk-interactive-test ros-indigo-jsk-recognition-utils
-#apt install -y ros-indigo-jsk-rqt-plugins ros-indigo-jsk-rviz-plugins
-#apt install -y ros-indigo-jsk-visualization ros-indigo-pcl-conversions ros-indigo-pcl-ros
 
 echo "***************************************"
 echo "*****       for AR Toolkit        *****"
@@ -94,46 +51,8 @@ wget https://github.com/MoriKen254/ARToolKit_Release/raw/master/ARToolKit_2.72.1
 yes|dpkg -i ARToolKit_2.72.1_Ubuntu_amd64.deb
 
 echo "***************************************"
-echo "*****          for Eigen          *****"
-echo "***************************************"
-echo "***** apt install libeigen3-dev *****"
-apt install libeigen3-dev
-echo "***** ll /usr/include | grep eigen *****"
-ll /usr/include | grep eigen
-echo "***** ll /usr/local/include | grep eigen *****"
-ll /usr/local/include | grep eigen
-
-echo "***************************************"
 echo "*****       Apply Lib Pkgs        *****"
 echo "***************************************"
-echo "***** echo CPATH *****"
-echo $CPATH
-echo "***** export CPATH=/usr/include:/usr/include/eigen3:/usr/local/include:CPATH *****"
-export CPATH=/usr/include:/usr/include/eigen3:/usr/local/include:$CPATH
-echo "***** echo CPATH *****"
-echo $CPATH
-echo "---------------------------------------"
-echo "***** echo C_INCLUDE_PATH *****"
-echo $C_INCLUDE_PATH
-echo "***** export C_INCLUDE_PATH=/usr/include:/usr/include/eigen3:/usr/local/include:C_INCLUDE_PATH *****"
-export C_INCLUDE_PATH=/usr/include:/usr/include/eigen3:/usr/local/include:$C_INCLUDE_PATH
-echo "***** echo C_INCLUDE_PATH *****"
-echo $C_INCLUDE_PATH
-echo "---------------------------------------"
-echo "***** echo CPLUS_INCLUDE_PATH *****"
-echo $CPLUS_INCLUDE_PATH
-echo "***** export CPLUS_INCLUDE_PATH=/usr/include:/usr/include/eigen3:/usr/local/include:CPLUS_INCLUDE_PATH *****"
-export CPLUS_INCLUDE_PATH=/usr/include:/usr/include/eigen3:/usr/local/include:$CPLUS_INCLUDE_PATH
-echo "***** echo CPLUS_INCLUDE_PATH *****"
-echo $CPLUS_INCLUDE_PATH
-echo "---------------------------------------"
-echo "***** echo LD_LIBRARY_PATH *****"
-echo $LD_LIBRARY_PATH
-echo "***** export LD_LIBRARY_PATH=/usr/local/lib:usr/lib:LD_LIBRARY_PATH *****"
-export LD_LIBRARY_PATH=/usr/local/lib:usr/lib:$LD_LIBRARY_PATH
-echo "***** echo LD_LIBRARY_PATH *****"
-echo $LD_LIBRARY_PATH
-echo "---------------------------------------"
 echo "***** ldconfig *****"
 ldconfig
 echo "***** ldconfig -p *****"
